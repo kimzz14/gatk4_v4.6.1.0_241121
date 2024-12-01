efeed90e72db28d53e61fcc351b9b5c9efc24fcc
kimzz14 <kimzz1004@gmail.com>
2024-12-01 14:29:06 +0900

    py

--- 01.VC_sample/pipe/step01_GATK-CombineGVCFs.py
+++ 01.VC_sample/pipe/step01_GATK-CombineGVCFs.py
@@ -2,15 +2,13 @@ command  = ["gatk --java-options '-Djava.io.tmpdir=./tmp' CombineGVCFs"]
 command += ['--reference ../reference/ref.fa']
 command += ['--output result/pooled.HaplotypeCaller.g.vcf']
 
-fin = open('../gVCF_LIST')
-for line in fin:
-    gVCF = line.rstrip('\n')
-    command += ['--variant ../result/' + gVCF]
-fin.close()
+import glob
+for fileName in glob.glob('gVCF/*vcf'):
+    command += ['--variant' + ' ' + fileName]
 
 command += ['1> result/pooled.HaplotypeCaller.g.vcf.log']
 command += ['2> result/pooled.HaplotypeCaller.g.vcf.err']
 
-fout = open('step01_GATK-CombineGVCFs.sh', 'w')
+fout = open('../pipe/step01_GATK-CombineGVCFs.sh', 'w')
 fout.write(' \\\n'.join(command) + '\n')
 fout.close()
