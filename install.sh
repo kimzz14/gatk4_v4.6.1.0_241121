d0f217c5ba442dd03317a84035d6c3fb16b91787
kimzz14 <kimzz1004@gmail.com>
2024-12-27 11:02:06 +0900

    install.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,15 @@ conda create -n gatk4_v4.6.1.0-241121
 
 source activate gatk4_v4.6.1.0-241121
 
-conda install gatk4 samtools bcftools bwa
+conda install gatk4=4.6.1.0=py310hdfd78af_0 samtools bcftools bwa
 #bioconda/noarch::gatk4-4.6.1.0-py310hdfd78af_0
 
-conda install samtools
+#conda install samtools
 #bioconda/linux-64::samtools-1.21-h50ea8bc_0
 #bioconda/linux-64::htslib-1.21-h5efdd21_0
 
-conda install bcftools
+#conda install bcftools
 #bioconda/linux-64::bcftools-1.21-h8b25389_0
 
-conda install bwa
+#conda install bwa
 #bioconda/linux-64::bwa-0.7.18-he4a0461_1
